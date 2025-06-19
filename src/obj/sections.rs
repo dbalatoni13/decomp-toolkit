d61586a512c15f7439106d0cf5dcf4973fb539bf
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2025-06-19 22:24:38 +0200

    Heck for nfsmw

--- src/obj/sections.rs
+++ src/obj/sections.rs
@@ -236,7 +236,7 @@ impl ObjSection {
 
 pub fn section_kind_for_section(section_name: &str) -> Result<ObjSectionKind> {
     Ok(match section_name {
-        ".init" | ".text" | ".dbgtext" | ".vmtext" => ObjSectionKind::Code,
+        ".init" | ".text" | ".over" | ".dbgtext" | ".vmtext" => ObjSectionKind::Code,
         ".ctors" | ".dtors" | ".rodata" | ".sdata2" | "extab" | "extabindex" | ".BINARY" => {
             ObjSectionKind::ReadOnlyData
         }
